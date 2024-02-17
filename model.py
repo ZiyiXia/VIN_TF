cc9ecdd9f99ce41680be1edcfea6cd7eece5a527
ZiyiXia <joeyxia0805@163.com>
2024-02-17 17:43:39 -0500

    dataset and starting py files

---
+++ model.py
@@ -0,0 +1,15 @@
+import tensorflow as tf
+
+def VIT(X, S1, S2, args):
+    
+    k = args.k # Number of Value Iteration computations
+    ch_i = args.ch_i # Channels in input layer
+    ch_h = args.ch_h # Channels in initial hidden layer
+    ch_q = args.ch_q # Channels in q layer (~actions)
+    
+    pass
+
+
+def attention(tensor, args):
+    
+    pass
